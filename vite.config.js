11ba387b7cd79bb3745df9aa1f5bb0fd4ada3b58
inglagattuta <ing.lagattuta@gmail.com>
2025-11-14 00:48:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/lagportafoglio/", // <-- importantissimo! qui va il nome della tua repo
+  base: "/lagportafoglio/", 
 });

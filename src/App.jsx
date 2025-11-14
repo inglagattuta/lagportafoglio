87537c51b237c0ff79ee434539e872558817a8c3
inglagattuta <ing.lagattuta@gmail.com>
2025-11-14 01:13:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+<nav className="p-4 bg-gray-200 flex gap-4">
+  <Link to="/">Home</Link>
+  <Link to="/portfolio">Portfolio</Link>
+  <Link to="/score">Score</Link>
+  <Link to="/grafici">Grafici</Link>
+</nav>
+
 import { Outlet, Link } from "react-router-dom";
 
 export default function App() {

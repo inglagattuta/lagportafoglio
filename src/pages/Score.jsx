37901f2bfe76d9a3e64025ae31209c82103d4aa7
inglagattuta <ing.lagattuta@gmail.com>
2025-11-14 01:12:03 +0100

    Update Score.jsx

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,48 +1,21 @@
-import { useState } from "react";
-import { importExcel } from "../services/excel";
-
-
 export default function Score() {
-const [data, setData] = useState([]);
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Score Titoli</h1>
 
+      <p>Qui verranno mostrati i campi:</p>
+      <ul className="list-disc ml-6 mt-2">
+        <li>Nome</li>
+        <li>% 12 mesi</li>
+        <li>Rendimento%</li>
+        <li>Payback</li>
+        <li>% Portafoglio</li>
+        <li>Score</li>
+      </ul>
 
-const handleImport = async (e) => {
-const file = e.target.files[0];
-const imported = await importExcel(file);
-setData(imported);
-};
-
-
-return (
-<div>
-<h1>Score</h1>
-<input type="file" onChange={handleImport} />
-
-
-<table border="1" cellPadding="6" style={{ marginTop: 20 }}>
-<thead>
-<tr>
-<th>Nome</th>
-<th>% 12m</th>
-<th>Rendimento %</th>
-<th>Payback</th>
-<th>% Portafoglio</th>
-<th>Score</th>
-</tr>
-</thead>
-<tbody>
-{data.map((row, i) => (
-<tr key={i}>
-<td>{row.nome}</td>
-<td>{row.perc_12m}</td>
-<td>{row.rendimento}</td>
-<td>{row.payback}</td>
-<td>{row.portafoglio}</td>
-<td>{row.score}</td>
-</tr>
-))}
-</tbody>
-</table>
-</div>
-);
+      <p className="mt-4 text-gray-500">
+        (In seguito inseriamo la tabella collegata al tuo Excel.)
+      </p>
+    </div>
+  );
 }
